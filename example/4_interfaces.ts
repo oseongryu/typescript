fb34cf51502c2ec2d3f65c334b9010452752a1c8
oseongryu <oseongryu@gmail.com>
2022-11-09 09:06:06 +0900

    feat: class-note

---
+++ example/4_interfaces.ts
@@ -0,0 +1,54 @@
+// 인터페이스
+interface User {
+  name: string;
+  age: number;
+}
+
+// 변수에 사용하는 경우
+const seho: User = { name: '세호', age: 100 };
+
+// 함수의 매개변수에 사용하는 경우
+function getUser(user: User) {
+  console.log(user);
+}
+getUser(seho);
+
+// 함수의 전체 타입에 사용하는 경우
+interface SumFunction {
+  (a: number, b: number): number;
+}
+let sum: SumFunction;
+sum = function (num1: number, num2: string): number {
+  return num1 + num2;
+};
+
+// 배열의 인덱싱에 사용하는 경우
+interface StringArray {
+  [index: number]: string;
+}
+let arr: StringArray;
+arr[0] = 'hi';
+arr[1] = 10;
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+  [key: string]: RegExp;
+}
+
+const regexMap: StringRegexDictionary = {
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+};
+
+Object.keys(regexMap).forEach(function(value) {
+})
+
+// 인터페이스 확장
+interface Person {
+  name: string;
+  age: number; // 옵셔널 선택자 ? 동일하게 적용 가능
+}
+interface Developer extends Person {
+  language: string;
+}
+const joo: Developer = { name: 'joo', age: 20, language: 'ts' };
